b57dfd4139c8931109130a059ebdf34e7c0d9e8b
Siim Tiilen <eritikass@gmail.com>
2022-04-05 14:04:01 +0300

    errrrr

---
+++ prisma/migrations/20220405080556_broken/migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `Teacher` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `schoolId` INTEGER NOT NULL,
+
+    INDEX `Teacher_schoolId_idx`(`schoolId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
